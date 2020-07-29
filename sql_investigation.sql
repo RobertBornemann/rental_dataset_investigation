e3401cf0b84d7e282322737b42543d74b3bd2ebb
RobertBornemann <bornemann.robert@yahoo.de>
2020-07-29 12:54:04 +0200

    progress

--- sql_investigation.sql
+++ sql_investigation.sql
@@ -56,3 +56,53 @@ SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.length,
 FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
 JOIN film f ON fa.film_id = f.film_id
+
+-- Counting movies in each of the 4 filmlen_groups.
+SELECT filmlen_groups, COUNT(filmlen_groups) AS filmcount_bylencat
+FROM(
+		SELECT f.length,
+		 CASE 
+			WHEN f.length <= 60 THEN '<1h'
+			WHEN f.length > 60 AND f.length <= 120 THEN '1-2h'
+			WHEN f.length >= 120 AND f.length <= 180 THEN '2-3h'
+			WHEN f.length > 180 THEN '>3h'
+		 END AS filmlen_groups
+	FROM film f
+	) t1
+GROUP BY 1
+ORDER BY  filmlen_groups
+-- OR LIKE THIS
+SELECT DISTINCT(filmlen_groups), COUNT(title) OVER (PARTITION BY filmlen_groups) AS filmcount_bylencat
+FROM  (
+		SELECT f.title as title, f.length,
+		 CASE 
+			WHEN f.length <= 60 THEN '<1h'
+			WHEN f.length > 60 AND f.length <= 120 THEN '1-2h'
+			WHEN f.length >= 120 AND f.length <= 180 THEN '2-3h'
+			WHEN f.length > 180 THEN '>3h'
+		 END AS filmlen_groups
+	FROM film f
+	) t1
+ORDER BY  filmlen_groups
+
+-- We want to understand more about the movies that families are watching. 
+-- The following categories are considered family movies: Animation, Children, Classics, Comedy, Family and Music.
+-- Thus we create a query that lists each movie in family movies along with the film category it is classified in, and the number of times it has been rented out:
+
+SELECT *
+FROM (
+	SELECT f.title, c.name, COUNT(rental_id) as nr_times_rented,
+			CASE 
+				WHEN c.name IN('Animation', 'Children', 'Classics', 'Comedy', 'Family' , 'Music') THEN 'family_movies'
+				ELSE 'other'
+			END AS genre
+	FROM category c
+	JOIN film_category fc ON c.category_id = fc.category_id
+	JOIN film f ON f.film_id = fc.film_id
+	JOIN inventory i ON fc.film_id = i.film_id
+	JOIN rental r ON i.inventory_id = r.inventory_id
+	GROUP BY 1,2
+	)t1
+WHERE genre = 'family_movies'
+ORDER BY nr_times_rented DESC
+
