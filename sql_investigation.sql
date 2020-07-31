67e438dc1d87d2fa4a1831cea77fceaa4c23dc4d
RobertBornemann <bornemann.robert@yahoo.de>
2020-07-31 12:33:31 +0200

    finalizing the queries

--- sql_investigation.sql
+++ sql_investigation.sql
@@ -1,28 +1,27 @@
+-- Please find below my SQL queries from the "investigate a relation database" udacity project practice quizes and the question sets:
 
--- Getting started to explore the dataset:
+-- (Q1.1) Actors first and last name combined as full_name, film title, film description and length of the movie.
+SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.description, f.length
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id;
 
--- Actors first and last name combined as full_name, film title, film description and length of the movie.
+-- (Q1.2) Actors and movies where the movie length was more than 60 minutes.
 SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.description, f.length
 FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
 JOIN film f ON fa.film_id = f.film_id
+WHERE f.length > 60;
 
--- List of actors and movies where the movie length was more than 60 minutes.
-SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.description, f.length
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-JOIN film f ON fa.film_id = f.film_id
-WHERE f.length > 60
-
--- Capturing actor id, full name of the actor, and count the number of movies each actor has made. 
+-- (Q1.3) Capturing actor id, full name of the actor, and count the number of movies each actor has made. 
 SELECT a.actor_id, concat(a.first_name, ' ', a.last_name) as full_name, COUNT(f.film_id) as number_of_movies
 FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
 JOIN film f ON fa.film_id = f.film_id
 GROUP BY 1
-ORDER BY 2
+ORDER BY 3 DESC;
 
--- Actor with max & min number of movies.
+-- (EXTRA QUERY) Actor with max & min number of movies.
 SELECT *
 FROM (
 	SELECT a.actor_id, concat(a.first_name, ' ', a.last_name) as full_name, COUNT(f.film_id) as number_of_movies
@@ -43,9 +42,9 @@ FROM (
 	JOIN film f ON fa.film_id = f.film_id
 	GROUP BY 1
 	ORDER BY 3 
-	LIMIT 1) t2
+	LIMIT 1) t2;
 	
--- actor's full name, film title, length of movie + column name "filmlen_groups" that classifies movies based on their length (4 categories: <= 1h, 1-2h, 2-3h, >3h)
+-- (Q2.1) Actor's full name, film title, length of movie + column name "filmlen_groups" that classifies movies based on their length (4 categories: <= 1h, 1-2h, 2-3h, >3h)
 SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.length,
      CASE 
 	 	WHEN f.length <= 60 THEN '<1h'
@@ -55,9 +54,9 @@ SELECT concat(a.first_name, ' ', a.last_name) as full_name, f.title, f.length,
      END AS filmlen_groups
 FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
-JOIN film f ON fa.film_id = f.film_id
+JOIN film f ON fa.film_id = f.film_id;
 
--- Counting movies in each of the 4 filmlen_groups.
+-- (Q2.2) Counting movies in each of the 4 filmlen_groups.
 SELECT filmlen_groups, COUNT(filmlen_groups) AS filmcount_bylencat
 FROM(
 		SELECT f.length,
@@ -70,8 +69,9 @@ FROM(
 	FROM film f
 	) t1
 GROUP BY 1
-ORDER BY  filmlen_groups
--- OR LIKE THIS
+ORDER BY  filmlen_groups;
+
+-- (EXTRA QUERY) OR LIKE THIS
 SELECT DISTINCT(filmlen_groups), COUNT(title) OVER (PARTITION BY filmlen_groups) AS filmcount_bylencat
 FROM  (
 		SELECT f.title as title, f.length,
@@ -83,13 +83,9 @@ FROM  (
 		 END AS filmlen_groups
 	FROM film f
 	) t1
-ORDER BY  filmlen_groups
+ORDER BY  filmlen_groups;
 
--- We want to understand more about the movies that families are watching. 
--- The following categories are considered family movies: Animation, Children, Classics, Comedy, Family and Music.
--- Thus we create a query that lists each movie in family movies along with the film category it is classified in, and the number of times it has been rented out:
-
-
+-- QUESTION SET 1.1: Movie, the film category it is classified in, and the number of times it has been rented out.
 SELECT f.title, c.name, COUNT(rental_id) as nr_times_rented
 FROM category c
 JOIN film_category fc ON c.category_id = fc.category_id
@@ -100,40 +96,21 @@ GROUP BY 1,2
 HAVING c.name IN('Animation', 'Children', 'Classics', 'Comedy', 'Family' , 'Music')
 ORDER BY c.name;
 
--- Let's look at the length of rental duration of these family-friendly movies and compare it to the duration that all movies are rented for. 
--- We provide a table with the movie titles and divide them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter) 
--- based on the quartiles (25%, 50%, 75%) 
-
--- I tried this first:
-
-SELECT film_title, category, rental_duration,	
-	ntile(4) over (partition by rental_duration ) percentile
-FROM (
-	SELECT DISTINCT(f.title) AS film_title, c.name AS category, DATE_PART('day', r.return_date - r.rental_date) AS rental_duration 
-	FROM category c
-	JOIN film_category fc ON c.category_id = fc.category_id
-	JOIN film f ON f.film_id = fc.film_id
-	JOIN inventory i ON fc.film_id = i.film_id
-	JOIN rental r ON i.inventory_id = r.inventory_id
-	WHERE c.name IN('Animation', 'Children', 'Classics', 'Comedy', 'Family' , 'Music') 
-		AND DATE_PART('day', r.return_date - r.rental_date) > 0
-	)t1
-ORDER BY percentile;
-
--- but then I found out that there actually is a rental_rate column in the film table. That mean we can further simplyfy and refactor this query:
-
-SELECT DISTINCT(f.title) film_title, c.name category, f.rental_duration, 
-	ntile(4) OVER (ORDER BY rental_duration) standard_quartile
+-- QUESTION SET 1.2: Movie titles divided them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter) 
+	-- based on the quartiles (25%, 50%, 75%): 
+SELECT 	DISTINCT(f.title), 
+		c.name, 
+		f.rental_duration, 
+		ntile(4) OVER (ORDER BY rental_duration) standard_quartile
 FROM film f 
 JOIN film_category fc ON f.film_id = fc.film_id
 JOIN category c ON fc.category_id = c.category_id
 WHERE c.name IN('Animation', 'Children', 'Classics', 'Comedy', 'Family' , 'Music') 
-ORDER BY 3,4
+ORDER BY 3,1;
 
--- let's create a table with the family-friendly film category, each of the quartiles, and the corresponding count of movies within each combination of film category
--- for each corresponding rental duration category. The resulting table should have three columns:
-
-SELECT category, standard_quartile, COUNT(standard_quartile)
+-- QUESTION SET 1.3: Family-friendly film category, each of the quartiles, and the corresponding count of movies within each 
+	-- combination of film category for each corresponding rental duration category:
+SELECT category AS name, standard_quartile, COUNT(standard_quartile)
 FROM (
 	SELECT DISTINCT(f.title) film_title, c.name category, f.rental_duration, 
 		ntile(4) OVER (ORDER BY rental_duration) standard_quartile
@@ -144,48 +121,67 @@ FROM (
 	ORDER BY 3,4
 	)t1
 GROUP BY 1,2
-ORDER BY 1,2
+ORDER BY 1,2;
 
--- We want to find out how the two stores compare in their count of rental orders during every month for all the years we have data for. Write a query that returns the 
--- store ID for the store, the year and month and the number of rental orders each store has fulfilled for that month. Your table should include a column for each of the 
--- following: year, month, store ID and count of rental orders fulfilled during that month.
-	
-SELECT DATE_PART('month', r.rental_date), DATE_PART('year', r.rental_date), s.store_id, COUNT(DISTINCT r.rental_id)
-FROM store s
-JOIN staff st ON s.store_id = st.store_id
-JOIN payment py ON st.staff_id = py.staff_id
-JOIN rental r ON py.rental_id = r.rental_id
+-- QUESTION SET 2.1: Store ID for the store, the year and month and the number of rental orders each store has fulfilled for that month:
+SELECT 	DATE_PART('month', r.rental_date) AS Rental_month, 
+		DATE_PART('year', r.rental_date) AS Rental_year,
+		store_id AS Store_ID,
+		COUNT(r.rental_id) AS Count_rental
+FROM rental r
+JOIN staff st ON r.staff_id = st.staff_id
 GROUP BY 1,2,3
-ORDER BY 4 DESC
+ORDER BY 4 DESC;
 
-SELECT DATE_PART('month', r.rental_date), DATE_PART('year', r.rental_date), s.store_id AS store, 
-	COUNT(r.rental_id) OVER (PARTITION BY s.store_id ORDER BY DATE_PART('YEAR', r.rental_date))
-FROM store s
-JOIN staff st ON s.store_id = st.store_id
-JOIN payment py ON st.staff_id = py.staff_id
-JOIN rental r ON py.rental_id = r.rental_id
-ORDER BY 4 DESC
-
--- We would like to know who were our top 10 paying customers, how many payments they made on a monthly basis during 2007, and what was the amount of the monthly payments. 
--- Can you write a query to capture the customer name, month and year of payment, and total payment amount for each month by these top 10 paying customers?
-
-SELECT DATE_TRUNC('month', payment_date) AS pay_mon, concat(c.first_name, ' ', c.last_name) AS fullname, COUNT(amount)  AS pay_counterpermon, sum(amount) as pay_amount 
+-- QUESTION SET 2.2: Customer name, month and year of payment, and total payment amount for each month by the top 10 paying customers:
+SELECT 	DATE_TRUNC('month', payment_date) AS pay_mon, 	
+		concat(c.first_name, ' ', c.last_name) AS fullname, 
+		COUNT(amount) AS pay_counterpermon, 
+		sum(amount) AS pay_amount 
 FROM payment 
 JOIN customer c ON payment.customer_id = c.customer_id
 WHERE c.customer_id IN(
-	SELECT customer_id
-	FROM (
-		SELECT py.customer_id as customer_id, SUM(py.amount) AS pay_amount
-		FROM payment py
-		WHERE DATE_PART('year', py.payment_date) = 2007
-		GROUP BY 1
-		ORDER BY 2 DESC
-		LIMIT 10
-		)t2)
+		SELECT customer_id
+		FROM (
+				SELECT py.customer_id as customer_id, SUM(py.amount) AS pay_amount
+				FROM payment py
+				WHERE DATE_PART('year', py.payment_date) = 2007
+				GROUP BY 1
+				ORDER BY 2 DESC
+				LIMIT 10
+			)t1
+		)
 GROUP BY 1,2
-ORDER BY 2,1
+ORDER BY 2,1;
 
--- Finally, for each of these top 10 paying customers, I would like to find out the difference across their monthly payments during 2007. 
--- Please go ahead and write a query to compare the payment amounts in each successive month. Repeat this for each of these 10 paying customers. 
--- Also, it will be tremendously helpful if you can identify the customer name who paid the most difference in terms of payments.
-
+-- QUESTION SET 2.3: Comparing the payment amounts in each successive month for the Top 10 paying customers. 
+-- >>> Then displaying the customer name who paid the highest "lead" difference in terms of payments:
+SELECT 	fullname, pay_mon,
+		MAX(lead_difference) OVER (PARTITION BY fullname) AS max_lead_difference
+FROM (
+		SELECT pay_mon, fullname, pay_amount, 
+				LEAD(pay_amount) OVER (ORDER BY fullname) - pay_amount AS lead_difference
+		FROM (
+				SELECT 	DATE_TRUNC('month', payment_date) AS pay_mon, 
+						concat(c.first_name, ' ', c.last_name) AS fullname, 
+						COUNT(amount)  AS pay_counterpermon, 
+						sum(amount) AS pay_amount 
+				FROM payment 
+				JOIN customer c ON payment.customer_id = c.customer_id
+				WHERE c.customer_id IN(
+					SELECT customer_id
+					FROM (
+						SELECT py.customer_id as customer_id, SUM(py.amount) AS pay_amount
+						FROM payment py
+						WHERE DATE_PART('year', py.payment_date) = 2007
+						GROUP BY 1
+						ORDER BY 2 DESC
+						LIMIT 10
+						)t1
+					)
+				GROUP BY 1,2
+				ORDER BY 2,1
+				)t2
+		)t3
+ORDER BY 3 DESC
+LIMIT 1;
