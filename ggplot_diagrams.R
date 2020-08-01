0be643eac928d60a0a045a7b57fce13e96be9882
RobertBornemann <bornemann.robert@yahoo.de>
2020-08-01 13:26:02 +0200

    adding ggplot library

--- ggplot_diagrams.R
+++ ggplot_diagrams.R
@@ -1,20 +1,21 @@
 library(ggplot2)
+library("wesanderson")
 
 #theme sets:
 theme_set(theme_classic())
 theme_set(theme_bw())
 
-#load all csv files generated from the SQL queries:
-data_one = read.csv("sheet_one.csv")
-data_two = read.csv("sheet_two.csv")
-data_three = read.csv("sheet_three.csv")
-data_four = read.csv("sheet_four.csv")
-data_five = read.csv("sheet_five.csv")
-options(warn=-1)
+# load all csv files generated from the SQL queries:
+data_one = read.csv("csv_from_sql_files/sheet_one.csv")
+data_two = read.csv("csv_from_sql_files/sheet_two.csv")
+data_three = read.csv("csv_from_sql_files/sheet_three.csv")
+data_four = read.csv("csv_from_sql_files/sheet_four.csv")
+data_five = read.csv("csv_from_sql_files/sheet_five.csv")
 
 # total number of rentals grouped by year/month and colored by store id
 ggplot(data_one, aes(fill=as.factor(Store.ID), y=Rental, x=Year)) + 
   geom_bar(position="dodge", stat="identity") +
+  scale_fill_manual(values = wes_palette("Zissou1")) +
   labs(title = "Total Rental Orders By Shop") +
   ylab("Total Number Of Rentals") +
   xlab("Year And Month") +
@@ -23,6 +24,7 @@ ggplot(data_one, aes(fill=as.factor(Store.ID), y=Rental, x=Year)) +
 # count quartiles of rental duration by category colored by quartiles
 ggplot(data_two, aes(fill=as.factor(standard_quartile), y=rental_duration, x=category)) + 
   geom_bar(position="dodge", stat="identity") +
+  scale_fill_manual(values = wes_palette("Zissou1")) +
   labs(title = "Quartiles Of Rental Duration By Category") +
   ylab("Count") +
   xlab("Category") +
@@ -30,19 +32,20 @@ ggplot(data_two, aes(fill=as.factor(standard_quartile), y=rental_duration, x=cat
 
 # total rentals by category
 ggplot(data_four, aes(x=category, y=nr_times_rented))+
-  geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
+  geom_bar(stat="identity") +
+  scale_fill_manual(values = wes_palette("Zissou1")) +
   labs(title="Total Rentals By Category") +
   theme(axis.text.x = element_text(angle=65, vjust=0.6))
 
 # Total spend by month and top 10 customer
 ggplot(data_three, aes(x=name, y=date, label=dollar_spent)) + 
-  geom_point(stat='identity', fill="tomato2", size=11)  +
+  geom_point(stat='identity', fill="tomato2", size=11)+
   geom_segment(aes(y = 0, 
                    x = name, 
                    yend = date, 
                    xend = name), 
-               color = "black") +
-  geom_text(color="white", size=2.5) +
+               color = 'black') +
+  geom_text(color='white', size=2.5) +
   theme(axis.text.x = element_text(angle=65, vjust=0.6)) +
   ylab("Year / Month") +
   xlab("Top 10 Customers") 
